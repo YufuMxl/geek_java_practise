662f0540b69b782cec414a404658cf3eaaaef08d
xiaolongma <xiaolongma@yufuid.com>
2022-04-07 15:55:33 +0800

    无限创建线程

---
+++ homework/src/main/java/second_week/JavaVMStackOOM.java
@@ -0,0 +1,26 @@
+package second_week;
+
+/**
+ * VM Args：-Xss10M（该值越大，越容易 OOM）
+ */
+public class JavaVMStackOOM {
+    private void dontStop() {
+        while (true) {
+        }
+    }
+
+    public void stackLeakByThread() {
+        while (true) {
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {dontStop();}
+            });
+            thread.start();
+        }
+    }
+
+    public static void main(String[] args) {
+        JavaVMStackOOM oom = new JavaVMStackOOM();
+        oom.stackLeakByThread();
+    }
+}
