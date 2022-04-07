0a0e5fdc6ca1effefdacda0eb17600eec2445135
xiaolongma <xiaolongma@yufuid.com>
2022-04-07 17:31:59 +0800

    本地内存溢出

---
+++ homework/src/main/java/second_week/DirectMemoryOOM.java
@@ -0,0 +1,24 @@
+package second_week;
+
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
+/**
+ * VM Args：-Xmx20m  -XX:MaxDirectMemorySize=128k
+ * <p>
+ * MaxDirectMemorySize 默认与 Xmx 大小一致
+ */
+public class DirectMemoryOOM {
+
+    private static final int _1MB = 1024 * 1024;
+
+    public static void main(String[] args) throws Exception {
+        Field unsafeField = Unsafe.class.getDeclaredFields()[0];
+        unsafeField.setAccessible(true);
+        Unsafe unsafe = (Unsafe) unsafeField.get(null);
+        while (true) {
+            unsafe.allocateMemory(_1MB);
+        }
+    }
+}
