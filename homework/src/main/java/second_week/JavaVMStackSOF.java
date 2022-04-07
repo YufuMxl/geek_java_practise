63e77d7d1cd7fa99a9638a14791a5b23463683a2
xiaolongma <xiaolongma@yufuid.com>
2022-04-07 14:48:34 +0800

    虚拟机栈 StackOverflowError

---
+++ homework/src/main/java/second_week/JavaVMStackSOF.java
@@ -0,0 +1,24 @@
+package second_week;
+
+/**
+ * VM Args：-Xss160k
+ */
+public class JavaVMStackSOF {
+
+    private int stackLength = 1;
+
+    public void stackLeak() {
+        stackLength++;
+        stackLeak();
+    }
+
+    public static void main(String[] args) {
+        JavaVMStackSOF oom = new JavaVMStackSOF();
+        try {
+            oom.stackLeak();
+        } catch (Throwable e) {
+            System.out.println("stack length: " + oom.stackLength);
+            throw e;
+        }
+    }
+}
