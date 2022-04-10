b7eb51f6e8347ceb43ae8c417863f47c89fe23b3
xiaolongma <xiaolongma@yufuid.com>
2022-04-10 19:00:40 +0800

    更新守护线程代码

--- homework/src/main/java/third_week/DaemonThreadDemo.java
+++ homework/src/main/java/third_week/DaemonThreadDemo.java
@@ -1,8 +1,8 @@
 package third_week;
 
-public class ThreadDemo {
+public class DaemonThreadDemo {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Runnable task = () -> {
             try {
                 Thread.sleep(5000);
@@ -13,11 +13,12 @@ public class ThreadDemo {
             System.out.println("当前线程：" + currentThread.getName());
         };
 
+        // 创建守护线程
         Thread thread = new Thread(task);
         thread.setName("test-thread-1");
         thread.setDaemon(true);
+        // 运行守护线程
         thread.start();
-//        Thread.sleep(6000);
     }
 
 }
