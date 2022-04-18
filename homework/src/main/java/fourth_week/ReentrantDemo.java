9b3d9b41019d803bf4b4165acb1f2fe925188ca2
xiaolongma <xiaolongma@yufuid.com>
2022-04-18 16:24:19 +0800

    可重入锁测试

--- homework/src/main/java/fourth_week/ReentrantDemo.java
+++ homework/src/main/java/fourth_week/ReentrantDemo.java
@@ -1,5 +1,8 @@
 package fourth_week;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ReentrantDemo {
 
     private static final Object lock1 = new Object();
@@ -17,5 +20,26 @@ public class ReentrantDemo {
             }
         };
         new Thread(task).start();
+
+        final Lock lock = new ReentrantLock();
+        Runnable task2 = () -> {
+            try {
+                lock.lock();
+                System.out.println("当前线程对 lock1 加了第一层锁");
+
+                lock.lock();
+                System.out.println("当前线程对 lock1 加了第二层锁");
+
+                lock.lock();
+                System.out.println("当前线程对 lock1 加了第三层锁");
+            } finally {
+                // 加多少次锁，解多少次锁
+                lock.unlock();
+                lock.unlock();
+                lock.unlock();
+            }
+        };
+        new Thread(task2).start();
+        new Thread(task2).start();
     }
 }
