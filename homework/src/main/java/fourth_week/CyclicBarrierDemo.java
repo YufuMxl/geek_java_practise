bf97030d82998f4fd02f5e25a9aa568f8c8d1f5e
xiaolongma <xiaolongma@yufuid.com>
2022-05-04 10:54:00 +0800

    CyclicBarrierDemo

--- homework/src/main/java/fourth_week/CyclicBarrierDemo.java
+++ homework/src/main/java/fourth_week/CyclicBarrierDemo.java
@@ -10,15 +10,18 @@ public class CyclicBarrierDemo {
         CyclicBarrier cyclicBarrier = new CyclicBarrier(threadNum, () -> System.out.println(threadNum + " 个线程到齐"));
 
         final Runnable runnable = () -> {
+            String threadName = Thread.currentThread().getName();
             try {
-                System.out.println(Thread.currentThread().getName() + "线程等待其他线程");
+                System.out.println(threadName + "线程等待其他线程");
                 cyclicBarrier.await();
-                System.out.println(Thread.currentThread().getName() + "线程继续运行");
-            } catch (InterruptedException | BrokenBarrierException ignored) {
+                System.out.println(threadName + "线程继续运行");
+            } catch (InterruptedException | BrokenBarrierException e) {
+                System.out.println(threadName + " 出现 BrokenBarrier 异常，由 cyclicBarrier.reset() 导致");
             }
         };
 
-        for (int i = 0; i < threadNum; i++) {
+        for (int i = 0; i < 99; i++) {
+            System.out.println("等待的线程数为：" + cyclicBarrier.getNumberWaiting());
             new Thread(runnable).start();
             Thread.sleep(1000);
         }
